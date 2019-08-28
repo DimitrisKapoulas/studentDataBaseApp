67108d9abcd3abdc07a4a4772fd7ce15a631291a
DimitrisKapoulas <kapoulasd@gmail.com>
2019-08-28 23:19:00 +0300

    First check

--- src/studentDBApp/student.java
+++ src/studentDBApp/student.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 public class student {
     private String firstName;
     private String lastName;
-    private String gradeYear;
+    private int gradeYear;
     private int studentID;
-    private String courses;.
+    private String courses;
     private int tuitionBalance;
     private int costOfCourse = 600;
 
@@ -20,9 +20,9 @@ public class student {
         System.out.println("Enter student last name: ");
         this.lastName = scanner.nextLine();
 
-        System.out.println("Enter student class level: ");
-        this.firstName = scanner.nextLine();
-
+        System.out.println("1 - Freshman\n2 - Softmore\n3 - Junior\n4 - Senior\nEnter student class level: ");
+        this.gradeYear = scanner.nextInt();
+        System.out.println(firstName+" "+lastName+" "+gradeYear);
     }
 
     // Generate an ID
--- src/studentDBApp/studentDBApp.java
+++ src/studentDBApp/studentDBApp.java
@@ -2,6 +2,7 @@ package studentDBApp;
 
 public class studentDBApp {
     public static void main(String[] args) {
+        student stu1 = new student();
         // Ask how many new users we want to add
 
         // Create n number of new students
