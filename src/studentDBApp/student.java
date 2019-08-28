ec9a0449dbb2cfce1c983d87d26332b323863fd8
DimitrisKapoulas <kapoulasd@gmail.com>
2019-08-28 23:08:37 +0300

    Initialize constructor, plus setting guidelines

--- src/studentDBApp/student.java
+++ src/studentDBApp/student.java
@@ -1,4 +1,37 @@
 package studentDBApp;
 
+import java.util.Scanner;
 public class student {
+    private String firstName;
+    private String lastName;
+    private String gradeYear;
+    private int studentID;
+    private String courses;.
+    private int tuitionBalance;
+    private int costOfCourse = 600;
+
+    // Constructor: prompt user to enter student's name and year
+
+    public student() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter student first name: ");
+        this.firstName = scanner.nextLine();
+
+        System.out.println("Enter student last name: ");
+        this.lastName = scanner.nextLine();
+
+        System.out.println("Enter student class level: ");
+        this.firstName = scanner.nextLine();
+
+    }
+
+    // Generate an ID
+
+    // Enroll in courses
+
+    // View Balance
+
+    // Pay tuition
+
+    // Show status
 }
--- src/studentDBApp/studentDBApp.java
+++ src/studentDBApp/studentDBApp.java
@@ -2,6 +2,9 @@ package studentDBApp;
 
 public class studentDBApp {
     public static void main(String[] args) {
+        // Ask how many new users we want to add
+
+        // Create n number of new students
 
     }
 }
