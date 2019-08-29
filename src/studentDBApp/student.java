16e835cc2527e12612d78dc3474e4963e4986464
DimitrisKapoulas <kapoulasd@gmail.com>
2019-08-29 12:24:44 +0300

    Implement setStudentID method

--- src/studentDBApp/student.java
+++ src/studentDBApp/student.java
@@ -1,15 +1,16 @@
 package studentDBApp;
 
 import java.util.Scanner;
+
 public class student {
     private String firstName;
     private String lastName;
     private int gradeYear;
-    private int studentID;
+    private String studentID;
     private String courses;
     private int tuitionBalance;
-    private int costOfCourse = 600;
-
+    private static int costOfCourse = 600;
+    private static int id = 1000;
     // Constructor: prompt user to enter student's name and year
 
     public student() {
@@ -22,10 +23,17 @@ public class student {
 
         System.out.println("1 - Freshman\n2 - Softmore\n3 - Junior\n4 - Senior\nEnter student class level: ");
         this.gradeYear = scanner.nextInt();
-        System.out.println(firstName+" "+lastName+" "+gradeYear);
+
+        setStudentID();
+        System.out.println(firstName+" "+lastName+" "+gradeYear + " " + studentID);
     }
 
     // Generate an ID
+    private void setStudentID(){
+        // Grade Level + ID
+        id++;
+        this.studentID = gradeYear + "" +id;
+    }
 
     // Enroll in courses
 
