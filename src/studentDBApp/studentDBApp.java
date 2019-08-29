4e65c852a6cde11303b04328705727ef41fcca31
DimitrisKapoulas <kapoulasd@gmail.com>
2019-08-29 15:30:48 +0300

    Putting students into array

--- src/studentDBApp/studentDBApp.java
+++ src/studentDBApp/studentDBApp.java
@@ -1,14 +1,21 @@
 package studentDBApp;
 
+import java.util.Scanner;
+
 public class studentDBApp {
     public static void main(String[] args) {
-        student stu1 = new student();
-        stu1.enroll();
-        stu1.payTuition();
-        System.out.println(stu1.showInfo());
         // Ask how many new users we want to add
+        System.out.println("Enter number of new students to enroll: ");
+        Scanner in = new Scanner(System.in);
+        int numOfStudents = in.nextInt();
+        student[] students = new student[numOfStudents];
 
         // Create n number of new students
-
+        for (int i = 0; i < numOfStudents ; i++) {
+            students[i] = new student();
+            students[i].enroll();
+            students[i].payTuition();
+            System.out.println(students[i].showInfo());
+        }
     }
 }
