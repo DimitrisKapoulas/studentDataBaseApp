65ff31ca94e0cd608d191af6b3148c8e99723343
DimitrisKapoulas <kapoulasd@gmail.com>
2019-08-29 13:12:33 +0300

    Implement enroll method

--- src/studentDBApp/student.java
+++ src/studentDBApp/student.java
@@ -7,8 +7,8 @@ public class student {
     private String lastName;
     private int gradeYear;
     private String studentID;
-    private String courses;
-    private int tuitionBalance;
+    private String courses = null;
+    private int tuitionBalance = 0;
     private static int costOfCourse = 600;
     private static int id = 1000;
     // Constructor: prompt user to enter student's name and year
@@ -36,6 +36,22 @@ public class student {
     }
 
     // Enroll in courses
+    public void enroll(){
+        // Get inside a loop, user hits 0
+        do {
+            System.out.println("Enter course to enroll (Q to quit): ");
+            Scanner in = new Scanner(System.in);
+            String course = in.nextLine();
+            if (!course.equals("Q")) {
+                courses = courses + "\n" + course;
+                tuitionBalance += costOfCourse;
+            }else{
+                break;
+            }
+        }while ( 1 != 0);
+        System.out.println("Enrolled in: " + courses);
+        System.out.println("Tuition Balance: " + tuitionBalance);
+    }
 
     // View Balance
 
--- src/studentDBApp/studentDBApp.java
+++ src/studentDBApp/studentDBApp.java
@@ -3,6 +3,7 @@ package studentDBApp;
 public class studentDBApp {
     public static void main(String[] args) {
         student stu1 = new student();
+        stu1.enroll();
         // Ask how many new users we want to add
 
         // Create n number of new students
